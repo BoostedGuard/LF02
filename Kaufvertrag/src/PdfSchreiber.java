b6445972e69b5566e25b4a40a64a12f02cc82b7d
Lucas <lucas.b1@schule.bremen.de>
2022-03-26 10:10:43 +0100

    Vertrag

---
+++ Kaufvertrag/src/PdfSchreiber.java
@@ -0,0 +1,9 @@
+import javax.swing.text.Document;
+import java.io.FileNotFoundException;
+
+public class PdfSchreiber {
+    public static void main(String[] args) throws FileNotFoundException {
+        String datei = "C:/Users/Lucas/OneDrive/Programmieren PDF/Kaufvertrag.pdf";
+        Document document = new Document();
+    }
+}
