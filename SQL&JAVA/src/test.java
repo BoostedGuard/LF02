ae721f4c7d8dcfd30e030616f5abd72c40f5628c
BoostedGuard <lucas.b1@schule.bremen.de>
2022-05-16 11:33:29 +0200

    fussball

---
+++ SQL&JAVA/src/test.java
@@ -0,0 +1,7 @@
+import dao.VertragspartnerDAO;
+
+public class test {
+    public static void main(String[] args) throws ClassNotFoundException {
+        VertragspartnerDAO vertragspartnerDAO = new VertragspartnerDAO();
+    }
+}
